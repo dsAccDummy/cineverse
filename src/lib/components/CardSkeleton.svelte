700411fa7a0853e9b8d21c656035ef659927c2db
DS Acc Dummy <ds.acc.dummy@gmail.com>
2026-03-09 00:33:47 +0700

    Initial commit: CineVerse movie discovery app

---
+++ src/lib/components/CardSkeleton.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col rounded-xl overflow-hidden bg-card border border-border/50">
+  <div class="aspect-[2/3] shimmer"></div>
+  <div class="p-3 space-y-2">
+    <div class="h-4 w-3/4 rounded shimmer"></div>
+    <div class="h-3 w-1/2 rounded shimmer"></div>
+  </div>
+</div>
